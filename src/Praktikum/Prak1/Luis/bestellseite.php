e15c49f30a3e371d67037c7c6fe03887fd5afdb2
Westermeier, Luis <luis.westermeier@stud.h-da.de>
2023-04-21 09:25:40 +0200

    convert file to php

--- src/Praktikum/Prak1/Luis/bestellseite.php
+++ src/Praktikum/Prak1/Luis/bestellseite.php
@@ -12,7 +12,7 @@
 
     <h1>Bestellung</h1>
 
-    <form action="https://echo.fbi.h-da.de/">
+    <form action="https://echo.fbi.h-da.de/" method="post">
         <article class="pizzaList">
             <h2>Speisekarte</h2>
 
@@ -35,16 +35,16 @@
         <section>
             <h2>Warenkorb</h2>
 
-            <select name="shoppingCart[]" size="5" multiple>    <!-- static data for demonstration purposes -->
-                <option>Pizza Salami</option>
+            <select name="shoppingCart[]" size="5" multiple tabindex="0"  >    <!-- static data for demonstration purposes -->
+                <option selected>Pizza Salami</option>
                 <option>Pizza Margherita</option>
                 <option>Pizza Hawaii</option>
             </select>
             <p>14,50€</p>  <!-- static data for demonstration purposes -->
-            <input type="text" name="addressInput" placeholder="Ihre Adresse"> <br />
-            <button type="reset" name="deleteAll">Alle Löschen</button>
-            <button type="button" name="deleteSelected">Auswahl Löschen</button>
-            <button type="submit">Bestellen</button>
+            <input type="text" name="addressInput" value="" placeholder="Ihre Adresse"> <br />
+            <input type='reset' value='Alle Löschen'>
+            <input type='button' value='Auswahl Löschen'>
+            <input type='submit' value='Bestellen'>
         </section>
 
     </form>
