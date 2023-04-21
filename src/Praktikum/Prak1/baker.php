e6c3857a220158a739cdc9381854af14adc8c725
Luis Westermeier <luis.westermeier@stud.h-da.de>
2023-04-21 17:41:53 +0200

    fix values of radio input

--- src/Praktikum/Prak1/baker.php
+++ src/Praktikum/Prak1/baker.php
@@ -19,33 +19,33 @@
                 <fieldset id="orderOneMargherita">
                     <legend accesskey="1">Bestellung 1 Pizza Margherita</legend>
                     <label for="bestellt">Bestellt</label>
-                    <input type="radio" id="bestellt" name="orderOneMargherita" value="bestellt" checked> <br />
+                    <input type="radio" id="bestellt" name="orderOneMargherita" value="ordered" checked> <br />
                     <label for="inOven">Im Ofen</label>
-                    <input type="radio" id="inOven" name="orderOneMargherita" value="Im Ofen"> <br />
+                    <input type="radio" id="inOven" name="orderOneMargherita" value="inOven"> <br />
                     <label for="done">Fertig</label>
-                    <input type="radio" id="done" name="orderOneMargherita" value="Fertig"> <br />
+                    <input type="radio" id="done" name="orderOneMargherita" value="done"> <br />
                 </fieldset>
             </article>
             <article>
                 <fieldset id="orderOneSalami">
                     <legend accesskey="2">Bestellung 1 Pizza Salami</legend>
                     <label for="bestellt2">Bestellt</label>
-                    <input type="radio" id="bestellt2" name="orderOneSalami" value="bestellt" checked> <br />
+                    <input type="radio" id="bestellt2" name="orderOneSalami" value="ordered" checked> <br />
                     <label for="inOven2">Im Ofen</label>
-                    <input type="radio" id="inOven2" name="orderOneSalami" value="Im Ofen"> <br />
+                    <input type="radio" id="inOven2" name="orderOneSalami" value="inOven"> <br />
                     <label for="done2">Fertig</label>
-                    <input type="radio" id="done2" name="orderOneSalami" value="Fertig"> <br />
+                    <input type="radio" id="done2" name="orderOneSalami" value="done"> <br />
                 </fieldset>
             </article>
             <article>
                 <fieldset id="order3Hawaii">
                     <legend accesskey="3">Bestellung 3 Pizza Hawaii</legend>
                     <label for="bestellt3">Bestellt</label>
-                    <input type="radio" id="bestellt3" name="order3Hawaii" value="bestellt" checked> <br />
+                    <input type="radio" id="bestellt3" name="order3Hawaii" value="ordered" checked> <br />
                     <label for="inOven3">Im Ofen</label>
-                    <input type="radio" id="inOven3" name="order3Hawaii" value="Im Ofen"> <br />
+                    <input type="radio" id="inOven3" name="order3Hawaii" value="inOven"> <br />
                     <label for="done3">Fertig</label>
-                    <input type="radio" id="done3" name="order3Hawaii" value="Fertig"> <br />
+                    <input type="radio" id="done3" name="order3Hawaii" value="done"> <br />
                 </fieldset>
             </article>
         </section>
